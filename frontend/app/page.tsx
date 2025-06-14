a2a441a8c1189965c880747fec18c577293900ac
tunaslim <tunaslim@gmail.com>
2025-06-14 12:12:03 +0300

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -221,7 +221,7 @@ export default function Home() {
       )}
 
       {quotes?.length > 0 && !label && (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {quotes
             .slice()
             .sort((a, b) => a.TotalPrice - b.TotalPrice)
