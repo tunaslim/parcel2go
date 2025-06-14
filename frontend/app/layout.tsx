c0141aa7aff0b2bf25c4396db3d0fa49fdfd5107
tunaslim <tunaslim@gmail.com>
2025-06-14 13:47:12 +0300

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -24,19 +24,6 @@ export default function HomePage() {
           }}
         />
       </Head>
-
-      {/* Main content */}
-      <main className="p-6">
-        <h1 className="text-3xl font-bold underline text-clifford text-center">
-          Hello world!
-        </h1>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
-          <div className="bg-blue-200 p-4">Quote 1</div>
-          <div className="bg-green-200 p-4">Quote 2</div>
-          <div className="bg-yellow-200 p-4">Quote 3</div>
-        </div>
-      </main>
     </>
   );
 }
